52fbc52bb5fbe9ad3d542d24ad906fc5daf72964
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2021-06-11 23:37:44 -0400

    updates

--- index.js
+++ index.js
@@ -11,34 +11,36 @@ const time = parseInt(process.env.time || 20)
 const logPath = path.join(__dirname, "log.txt")
 
 const errorNotif = (msg) => {
-
     notifier.notify({
         title: '⚠⚠⚠',
         message: `${msg}\n${error}`,
         icon: path.join(__dirname, 'exclamation.png'),
         sound: true
     });
+}
 
+const appendLog = (msg, successCallback) => {
+    fs.appendFile(logPath, msg, (err) => {
+        if(err){
+            errorNotif("Failed to append")
+        }
+        else{
+            successCallback()
+        }
+    })
 }
 
 const reminder = () => {
-
     notifier.notify({
         title: '🚨🚨🚨',
         message: `Your ${time > 1 ? time : "every"} minute reminder.`,
         icon: path.join(__dirname, 'exclamation.png'),
         sound: true
     }, (err, res) => {
-        fs.appendFile(logPath, `${new Date().toISOString()} -> Error: ${err}, Response: ${res}\n`, (err) => {
-            if(err){
-                errorNotif("Failed to append")
-            }
-            else{
-                console.log(`reminder every ${time} minute${time > 1 ? "s" : ""}`);
-            }
+        appendLog(`${new Date().toISOString()} -> Error: ${err}, Response: ${res}\n`, () => {
+            console.log(`reminder every ${time} minute${time > 1 ? "s" : ""}`);
         })
     });
-    
 }
 
 /**
@@ -46,11 +48,13 @@ const reminder = () => {
  */
 fs.open(logPath, "w+", (err, fd) => {
     if(!err){
-        try {
-            cron.schedule(`*/${time} * * * *`, reminder);
-        } catch (error) {
-            errorNotif("Failed to set up cron")
-        }   
+        appendLog(`${new Date().toISOString()} -> Started`, () => {
+            try {
+                cron.schedule(`*/${time} * * * *`, reminder);
+            } catch (error) {
+                errorNotif("Failed to set up cron")
+            }  
+        })
     }
     else{
         errorNotif("Failed to set up log")
